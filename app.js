396062bb71033833c9b2efb2c64e3faccd09b841
のびすけ <rsksound@gmail.com>
2017-01-06 05:18:04 +0900

    step2+error処理

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const PORT = 3000;
 
 http.createServer((req, res) => {
   fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
+    if (err) {
+      res.statusCode = 500;
+      res.end('Error!');
+    }
+    //ファイルの読み込みが成功したらHTMLを返す
+    else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+    }
    });
 }).listen(PORT);
  
