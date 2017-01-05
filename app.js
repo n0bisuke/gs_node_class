229a47cc753c7a4f706e9eb71abccc2eeb1efe8f
のびすけ <rsksound@gmail.com>
2017-01-06 05:22:56 +0900

    step3.1

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const fs = require('fs');
 const PORT = 3000;
 
 http.createServer((req, res) => {
-  fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end('Error!');
-    }
-    //ファイルの読み込みが成功したらHTMLを返す
-    else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(html);
-    }
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error!');
+      }
+      //ファイルの読み込みが成功したらHTMLを返す
+      else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+      }
    });
+  }
 }).listen(PORT);
  
 console.log(`Server running at http://localhost:${PORT}`);
