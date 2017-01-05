aadebec66d1f5c23fc1ef2f9e22a6867e1a32283
のびすけ <rsksound@gmail.com>
2017-01-06 05:15:00 +0900

    step2

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 'use strict'
 
 const http = require('http');
+const fs = require('fs');
 const PORT = 3000;
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="utf-8">
-    <title>ログイン</title>
-</head>
-<body>
-    <form method="post" action="/post">
-        <p>ユーザID:<input type="text" name="userid"></p>
-        <p>パスワード:<input type="password" name="password"></p>
-        <p><input type="submit"></p>
-    </form>
-</body>
-</html>`);
+  fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+   });
 }).listen(PORT);
  
 console.log(`Server running at http://localhost:${PORT}`);
