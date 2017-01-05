c09e0638935767a27bfebbc0d7a6b1fc31608e94
のびすけ <rsksound@gmail.com>
2017-01-06 05:48:12 +0900

    step5

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const http = require('http');
 const fs = require('fs');
 const PORT = 3000;
 
+const indexPage = (req, res) => {
+  fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end('Error!');
+    }
+    //ファイルの読み込みが成功したらHTMLを返す
+    else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+    }
+  });
+}
+
+const postPage = (req, res) => {
+  let body = '';
+  req.on('readable', (chunk) => {
+    body += req.read();
+  });
+  req.on('end', () => {
+    if(body === '' || body === null) return;
+    res.end(body);
+  });
+}
+
 http.createServer((req, res) => {
   // ルート(/)へのGETリクエストの時の処理
   if (req.url === '/' && req.method === 'GET') {
-    fs.readFile(__dirname + '/index.html', {encoding: 'utf8'}, (err, html) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end('Error!');
-      }
-      //ファイルの読み込みが成功したらHTMLを返す
-      else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
-      }
-   });
+    indexPage(req, res);
   }
   
   // /postPageへのpostリクエストの時の処理
   else if (req.url === '/postPage' && req.method === 'POST'){
-    let body = '';
-    req.on('readable', (chunk) => {
-        body += req.read();
-    });
-    req.on('end', () => {
-        if(body === '' || body === null) return;
-        res.end(body);
-    });
+   postPage(req, res);
   }
 
   // その他
